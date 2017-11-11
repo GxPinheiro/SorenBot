d3a4696a116c02eddc7631bae4f4b331e9262084
Pinheiro <gp12@my.fsu.edu>
2017-11-11 00:41:52 -0200

    Adicionada funcao boku hero e youtube

--- SorenBot.js
+++ SorenBot.js
@@ -3,8 +3,13 @@ const TeleBot = require('telebot');
 
 const bot = new TeleBot('413868618:AAGU_Ua75t9jqa0RxeUl2sDB4DuGuuUx-RE');
 
-bot.on('video', (msg) => {
+
+bot.on(/youtube.com/, (msg) => {
     return msg.reply.text ('seu gosto é um lixo mesmo', { asReply: true });
+});
+
+bot.on(/(boku\s)?hero academia*/, (msg) => {
+    return msg.reply.text ('Shonen Generico', { asReply: true });
 });
  
 bot.connect();
